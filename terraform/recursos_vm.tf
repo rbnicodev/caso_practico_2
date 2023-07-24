3d9094153d835a5bfb6d95f590a3e686320f4a7d
Rubén Muñoz <rubenmsanchis.dev@gmail.com>
2023-07-24 20:55:49 +0200

    vm funcionando con ssh

---
+++ terraform/recursos_vm.tf
@@ -0,0 +1,81 @@
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = var.vm_public_ip
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = var.vm_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = var.vm_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "vm_vnic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/azurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Debian"
+    offer     = "debian-10"
+    sku       = "10"
+    version   = "latest"
+  }
+}
+
+resource "null_resource" "ansible_installation" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y ansible"
+    ]
+  }
+
+  connection {
+    type     = "ssh"
+    host     = azurerm_linux_virtual_machine.vm.public_ip_address
+    user     = azurerm_linux_virtual_machine.vm.admin_username
+    agent    = true
+    private_key = file("~/.ssh/azurekey")
+    timeout  = "1m"
+  }
+
+  depends_on = [azurerm_linux_virtual_machine.vm]
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-cs2"
+}
+
+variable "location_name" {
+  default = "uksouth"
+}
+
+variable "vm_network_name" {
+  default = "vm_vnet"
+}
+
+variable "vm_subnet_name" {
+  default = "vm_subnet"
+}
+
+variable "vm_public_ip" {
+    default = "vm_public_ip"
+}
